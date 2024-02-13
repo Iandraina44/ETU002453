901fc2645934f7882ca762eff849037fd2a0d3a5
Mamy-Ny-Aina <ndimbyrazafinjatovo999@gmail.com>
2024-02-13 12:45:15 +0300

    ioooo

--- inc/fonction.php
+++ inc/fonction.php
@@ -84,14 +84,23 @@ function insertsalaire($idcueilleur, $montant) {
 }
 
 function insertcueillette($datecueillette, $idcueilleur, $idparcelle, $poids) {
-    $requette = "INSERT INTO cueillette VALUES (NULL, '%s', %d, %d, %.2f)";
-    $requette = sprintf($requette, $datecueillette, $idcueilleur, $idparcelle, $poids);
-    $result = mysqli_query(dbconnect(), $requette);
-    if ($result) {
-        echo "Insertion dans 'cueillette' réussie.";
-    } else {
-        echo "Erreur lors de l'insertion dans 'cueillette': " . mysqli_error(dbconnect());
+    $validation=true;
+    $poidrestant= poids_restant_parcelle_byid($datecueillette, $datecueillette, $idparcelle);
+    if ($poids>$poidrestant) {
+        $validation=false;
     }
+    else{
+        $requette = "INSERT INTO cueillette VALUES (NULL, '%s', %d, %d, %.2f)";
+        $requette = sprintf($requette, $datecueillette, $idcueilleur, $idparcelle, $poids);
+        $result = mysqli_query(dbconnect(), $requette);
+        if ($result) {
+            echo "Insertion dans 'cueillette' réussie.";
+        } else {
+            echo "Erreur lors de l'insertion dans 'cueillette': " . mysqli_error(dbconnect());
+        }
+    }
+    return $validation;
+   
 }
 
 function insertdepense($idcategoriedepense, $montant,$date) {
@@ -130,8 +139,6 @@ function getStatutPersonne($id){
     }   
     
 }
-
-
 
 
 function getAllThe() {
@@ -311,6 +318,37 @@ function poids_restant_parcelle() {
     }
     return $data;
 }
+
+function poids_restant_parcelle_byid($min, $max, $idParcelle) {
+    $db = dbconnect();
+
+    $query = "SELECT 
+                SUM((th.rendement * p.surface * 10000 / th.occupation)) - COALESCE(SUM(c.poids), 0) AS poids_restant
+              FROM 
+                parcelle p
+              JOIN 
+                the th ON p.idthe = th.idthe
+              LEFT JOIN 
+                cueillette c ON p.idparcelle = c.idparcelle
+              WHERE 
+                c.datecueillette <= '%s'
+                AND p.idparcelle = %d
+                AND MONTH(c.datecueillette) >= (SELECT idmois FROM saison WHERE idmois <= MONTH('%s') ORDER BY idmois DESC LIMIT 1);";
+
+    $query = sprintf($query, $max, $idParcelle, $max);
+
+    $result = mysqli_query($db, $query);
+    $data = array();
+    
+    if ($result && mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row['poids_restant'];
+        }
+    }
+    
+    return $data;
+}
+
 
 function poids_restant_parcelle_date($min, $max) {
     $db = dbconnect();
